c077d21b2c0b2df30a7254976253920fa5410c48
Uíliam Mateu Martins <100007488.joan23@fje.edu>
2022-10-10 16:54:25 +0200

    Arreglos validacion

--- proc/validacion.php
+++ proc/validacion.php
@@ -1,8 +1,7 @@
 <?php
-if (!empty($_POST['user']) && !empty($_POST['pass'])) {
+if (!empty($_POST['user']) && !empty($_POST['password'])) {
     function login($email_login, $password_login) {
         require "./conexion.php";
-
         if (!$conn) {
             echo "ERROR DE CONEXION CON LA BASE DE DATOS";
             echo "<a href='../view/login.php'>volver</a>";
@@ -10,24 +9,22 @@ if (!empty($_POST['user']) && !empty($_POST['pass'])) {
         }
         $user = $_POST['user'];
         $res = explode("@", $user);
-        $query = "SHOW GRANTS FOR $res[0]";
-        $result = mysqli_query($conn,$query);
-        print_r($result);
+        $querya = "SHOW GRANTS FOR $res[0]";
+        $result = mysqli_query($conn,$querya);
         if ($result == true){
-            $query = "SELECT * FROM tbl_gestornotas WHERE user = '{$email_login}' AND password = '{$password_login}'";
-            $valid_login = mysqli_query($conn, $query);
+            $query2 = "SELECT * FROM tbl_gestornotas WHERE user = '{$email_login}' AND password = '{$password_login}'";
+            $valid_login1 = mysqli_query($conn, $query2);
+            print_r($valid_login1);
             echo "está entrando?";
-            $match = $valid_login -> num_rows;
-            if ($match === 1) {
-                session_start();
-    echo "aqui tbm chega?";
-                foreach ($valid_login as $key => $user) {
+            $match1 = $valid_login1 -> num_rows;
+            print_r($match1);
+            if ($match1 === 1) {
+                foreach ($valid_login1 as $key => $user) {
                     $_SESSION['id_usuario'] = $user['id'];
                     $_SESSION['email_usuario'] = $user['user'];
-                    $_SESSION['contra_usuario'] = $user['pass'];
+                    $_SESSION['contra_usuario'] = $user['password'];
                 }
-              //  echo "<script>location.href = '../vistas/bienvenido.html';</script>";
-            } else {
+              echo "<script>location.href = '../vistas/bienvenidoAdmin.html';</script>";
             }
         }
         $query = "SELECT * FROM tbl_gestornotas WHERE user = '{$email_login}' AND password = '{$password_login}'";
@@ -40,19 +37,19 @@ if (!empty($_POST['user']) && !empty($_POST['pass'])) {
             foreach ($valid_login as $key => $user) {
                 $_SESSION['id_usuario'] = $user['id'];
                 $_SESSION['email_usuario'] = $user['user'];
-                $_SESSION['contra_usuario'] = $user['pass'];
+                $_SESSION['contra_usuario'] = $user['password'];
             }
-       //     echo "<script>location.href = '../vistas/bienvenido.html';</script>";
+            echo "<script>location.href = '../vistas/bienvenido.html';</script>";
         } else {
-       //     echo "<script>location.href = '../index.html?validation=false';</script>";
+           echo "<script>location.href = '../index.html?validation=false';</script>";
         }
     }
 
     $email = $_POST['user'];
-    $password = $_POST['pass'];
+    $password = $_POST['password'];
 
     login($email,$password);
 
 } else {
- //  echo "<script>location.href = '../index.html?validation=false';</script>";
+  echo "<script>location.href = '../index.html?validation=false';</script>";
 }
